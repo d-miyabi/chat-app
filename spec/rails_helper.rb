2e457dfe8302fc98e3985b35ed4fb97a18789d41
d-miyabi <msgh3110+works@gmail.com>
2024-01-05 23:30:41 +0900

    結合テストの実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,10 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+# コメントアウトを外す
+Rails.root.glob('spec/support/**/*.rb').sort.each { |f| require f }
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -33,6 +37,8 @@ I18n.locale = "en"
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = Rails.root.join('spec/fixtures')
+  config.include SignInSupport
+
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
